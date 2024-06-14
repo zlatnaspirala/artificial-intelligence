dbe02c7f128e0d5aa1c98c70f540e7af8d5a8eb1
zlatnaspirala <zlatnaspirala@gmail.com>
2024-06-15 00:42:52 +0200

    wip

--- tools/tensorflow/camera/camera.js
+++ tools/tensorflow/camera/camera.js
@@ -24,7 +24,6 @@
  ********************************************************************/
 
 const demosSection = document.getElementById('demos');
-
 var model = undefined;
 
 // Before we can use COCO-SSD class we must wait for it to finish
@@ -32,31 +31,18 @@ var model = undefined;
 // get everything needed to run.
 cocoSsd.load().then(function(loadedModel) {
 	model = loadedModel;
-	// Show demo section now model is ready to use.
 	demosSection.classList.remove('invisible');
 });
-
-/********************************************************************
-// Demo 2: Continuously grab image from webcam stream and classify it.
-// Note: You must access the demo on https for this to work:
-// https://tensorflow-js-image-classification.glitch.me/
-********************************************************************/
 
 const video = document.getElementById('webcam');
 const liveView = document.getElementById('liveView');
 
-// Check if webcam access is supported.
 function hasGetUserMedia() {
 	return !!(navigator.mediaDevices &&
 		navigator.mediaDevices.getUserMedia);
 }
 
-// Keep a reference of all the child elements we create
-// so we can remove them easilly on each render.
 var children = [];
-
-// If webcam supported, add event listener to button for when user
-// wants to activate it.
 if(hasGetUserMedia()) {
 	const enableWebcamButton = document.getElementById('webcamButton');
 	enableWebcamButton.addEventListener('click', enableCam);
@@ -64,22 +50,15 @@ if(hasGetUserMedia()) {
 	console.warn('getUserMedia() is not supported by your browser');
 }
 
-// Enable the live webcam view and start classification.
 function enableCam(event) {
 	if(!model) {
 		console.log('Wait! Model not loaded yet.')
 		return;
 	}
-
-	// Hide the button.
 	event.target.classList.add('removed');
-
-	// getUsermedia parameters.
 	const constraints = {
 		video: true
 	};
-
-	// Activate the webcam stream.
 	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
 		video.srcObject = stream;
 		video.addEventListener('loadeddata', predictWebcam);
@@ -87,16 +66,11 @@ function enableCam(event) {
 }
 
 function predictWebcam() {
-	// Now let's start classifying the stream.
 	model.detect(video).then(function(predictions) {
-		// Remove any highlighting we did previous frame.
 		for(let i = 0;i < children.length;i++) {
 			liveView.removeChild(children[i]);
 		}
 		children.splice(0);
-
-		// Now lets loop through predictions and draw them to the live view if
-		// they have a high confidence score.
 		for(let n = 0;n < predictions.length;n++) {
 			// If we are over 66% sure we are sure we classified it right, draw it!
 			if(predictions[n].score > 0.66) {
@@ -108,7 +82,6 @@ function predictWebcam() {
 				p.style = 'left: ' + predictions[n].bbox[0] + 'px;' +
 					'top: ' + predictions[n].bbox[1] + 'px;' +
 					'width: ' + (predictions[n].bbox[2] - 10) + 'px;';
-
 				// Draw the actual bounding box.
 				const highlighter = document.createElement('div');
 				highlighter.setAttribute('class', 'highlighter');
@@ -119,14 +92,10 @@ function predictWebcam() {
 
 				liveView.appendChild(highlighter);
 				liveView.appendChild(p);
-
-				// Store drawn objects in memory so we can delete them next time around.
 				children.push(highlighter);
 				children.push(p);
 			}
 		}
-
-		// Call this function again to keep predicting when the browser is ready.
 		window.requestAnimationFrame(predictWebcam);
 	});
 }
